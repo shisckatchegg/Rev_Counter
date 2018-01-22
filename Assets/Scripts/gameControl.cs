3597f1a4d5db094f862bb72982d0c9b917b89d80
shisckatchegg <alvaro_martinez_93@hotmail.com>
2018-01-22 23:32:00 +0000

    First commit

---
+++ Assets/Scripts/gameControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameControl
+{
+
+}
+
+public class TurnControl
+{
+	public CityStatProcesser _cityStats;
+
+	public UnitStatProcesser _unitStats;
+
+	public PlayerStatPRocesser _playerStats;
+
+	public void RefreshTurn()
+	{
+		_cityStats.Update();
+
+		_unitStats .Update();
+
+		_playerStats.Update();
+	}
+}
---
+++ Assets/Scripts/stats.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopulationStats
+{
+	public int Control;
+
+	public int Support;
+
+	public void SupportDecay()
+	{
+		if(Control > 60)
+		{
+			Support--;
+		}
+	}
+}
+
+
+public class stats : MonoBehaviour {
+
+	private PopulationStats _stats;
+
+	// Use this for initialization
+	void Start () {
+		_stats = new PopulationStats();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+}

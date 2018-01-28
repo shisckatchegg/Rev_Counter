ce0b04417710c3c7e2108c0ae1954b47851bd1a0
shisckatchegg <alvaro_martinez_93@hotmail.com>
2018-01-28 21:00:03 +0000

    Population node information display

---
+++ Assets/Scripts/PopulationNodeController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PopulationNodeController : MonoBehaviour {
+
+	private PopulationNodeStats _populationNodeStats;
+
+	private Text _populationNodeStatDisplay;
+
+	// Use this for initialization
+	void Start () {
+		_populationNodeStats = GetComponent<PopulationNodeStats>();
+		_populationNodeStatDisplay = GetComponentInChildren<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		_populationNodeStatDisplay.text = 
+			_populationNodeStats.Stats.PopulationNodeName 
+			+ "\nPopulation: " + _populationNodeStats.Stats.Population
+			+ "\nControl: " + _populationNodeStats.Stats.Control
+			+ "\nSupport: " + _populationNodeStats.Stats.FactionsSupport[0] + " " + _populationNodeStats.Stats.FactionsSupport[1];
+	}
+}

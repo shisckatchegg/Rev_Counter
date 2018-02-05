d798ea3d6d3fca7c60e16e24fea659349f5bf09d
alvaropfg <alvaropfg@gmail.com>
2018-02-05 21:42:27 +0100

    Units update

--- Assets/Scripts/UnitBase.cs
+++ Assets/Scripts/UnitBase.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class UnitBase : MonoBehaviour {
+    public int Faction;     //en PopulationNodeStats.cs se llama Control?
+    public string Location; //en PopulationNodeStats.cs se llama PopulationNodeName?
 
-	// Use this for initialization
-	void Start () {
-		
+    // Use this for initialization
+    void Start () {
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
 	}
 }

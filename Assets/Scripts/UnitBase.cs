c22792bed3a8a0c43a40ee3f6212fc38d3105320
alvaropfg <alvaropfg@gmail.com>
2018-02-06 11:14:18 +0100

    Units update

--- Assets/Scripts/UnitBase.cs
+++ Assets/Scripts/UnitBase.cs
@@ -6,10 +6,10 @@ public class UnitBase : MonoBehaviour {
     public int Faction;     //en PopulationNodeStats.cs se llama Control?
 
     public GameObject Location;
-
+    /*
     public UnitBase(GameObject location){
         Location = location;
-    }
+    }*/
 
     // Use this for initialization
     void Start () {
